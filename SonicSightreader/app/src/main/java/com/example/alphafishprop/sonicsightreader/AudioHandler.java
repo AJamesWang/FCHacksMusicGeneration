21e5ee905c211396f7e4c5ab04a0b63badb5eb68
zhendeveloper <zhenpublicrelations@gmail.com>
2017-03-25 13:29:43 -0400

    Temp changes to AudioHandler

--- SonicSightreader/app/src/main/java/com/example/alphafishprop/sonicsightreader/AudioHandler.java
+++ SonicSightreader/app/src/main/java/com/example/alphafishprop/sonicsightreader/AudioHandler.java
@@ -5,6 +5,9 @@ import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.util.Log;
 
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 /**
@@ -15,7 +18,8 @@ public class AudioHandler {
 
     private static final String LOG_TAG="AudioHandler";
 
-    private static String mFileName=null;//@TODO: give file a name
+    private FileDescriptor mRecordedAudioFileDesc = null;
+    private File mCorrectMelodyFile = null;
     private MediaRecorder mRecorder=null;
     private MediaPlayer mPlayer=null;
 
@@ -24,14 +28,16 @@ public class AudioHandler {
 //    private boolean permissionToRecordAccepted=false;
 //    private String[] permissions={Manifest.permission.RECORD_AUDIO};
 
-    public AudioHandler(){
+    public AudioHandler()
+    {
+        mRecordedAudioFileDesc = FileOutputStream(File.createTempFile("recordedAudio", "3gpp")).getFD()
     }
 
     public void startRecording(){
         mRecorder=new MediaRecorder();
         mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
         mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        mRecorder.setOutputFile(mFileName);
+        mRecorder.setOutputFile(mRecordedAudioFileDesc);
         mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
 
         try{
@@ -47,10 +53,10 @@ public class AudioHandler {
         mRecorder=null;
     }
 
-    public void startPlayback(){
+    public void startPlaybackOfRecordedVoice(){
         mPlayer=new MediaPlayer();
         try {
-            mPlayer.setDataSource(mFileName);
+            mPlayer.setDataSource(mRecordedAudioFileDesc);
             mPlayer.prepare();
             mPlayer.start();
         } catch(IOException e){
@@ -58,8 +64,22 @@ public class AudioHandler {
         }
     }
 
-    public void stopPlayback(){
+    public void stopPlaybackOfRecordedVoice(){
         mPlayer.release();
         mPlayer=null;
     }
+
+    /*
+    public void startPlaybackOfCorrectMelody(List<String> melody)
+    {
+        mCorrectMelodyFile = File.createTempFile("correctMelody", "mid");
+        AudioFileWriter.writeMelodyToFile(melody, FileOutputStream(mCorrectMelodyFile));
+
+    }
+
+    public void stopPlaybackOfCorrectMelody()
+    {
+
+    }
+    */
 }
